cb84cb2dca5e37c2bccf62a22b0b50a35c612162
James World <james-world@users.noreply.github.com>
2017-11-14 00:14:30 +0000

    Add swashbuckle

--- src/Twitbook.Api/Startup.cs
+++ src/Twitbook.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Twitbook.Api
 {
@@ -24,6 +25,11 @@ namespace Twitbook.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddXmlDataContractSerializerFormatters();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "Twitbook API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +39,8 @@ namespace Twitbook.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
 
             app.UseMvc();
         }
